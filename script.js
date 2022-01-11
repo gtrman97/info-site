f4b12b3670333ccf8105b0e47592c95aa04a2fdc
gtrman97 <andrew.killian@g.austincc.edu>
2022-01-11 03:02:40 -0600

    added react code

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ function MainContent(){
     )
 }
 
+let Section = () => (
+    <p>It is fun!</p>
+)
 
 ReactDOM.render(
 <div>
 <MainContent />
+<Section />
 </div>, document.getElementById("root"));
+
+// let root = document.getElementById("root"), h1 = document.createElement("h1");
+// h1.textContent("Imperative");
+// h1.className = "header";
+// root.append(h1);
